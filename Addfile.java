f9a45e5c6dd3bf13e82406c5a7e27653f9ed9b2a
madhaviGit <projects.madhavi@googlemail.com>
2016-10-27 11:37:28 +0200

    second commit

---
+++ Addfile.java
@@ -0,0 +1,8 @@
+package com.example.madhavi.newgit;
+
+/**
+ * Created by Madhavi on 27.10.2016.
+ */
+public class Addfile {
+    int g;
+}
--- MainActivity.java
+++ MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Addfile f=new Addfile();
+        f.g=12;
+
     }
 
     private void add()
